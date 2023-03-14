28a34f980e4ded8245c76b2180fa794a6e45426f
lingyun-z <zhanglingyun19970823@gmail.com>
2023-03-14 19:03:35 +0800

    1

--- setup.sh
+++ setup.sh
@@ -4,4 +4,3 @@ docker network create v2ray
 
 docker build -f config.Dockerfile -t v2raysetup_config:latest . && \
 docker run --env-file .env -p 80:80 -v /etc/v2ray:/etc/v2ray -v /etc/nginx:/etc/nginx -v /etc/haproxy:/etc/haproxy -d v2raysetup_config:latest sh gen-config.sh
-docker run -v /etc/haproxy:/usr/local/etc/haproxy -v /etc/v2ray:/etc/ssl/private -it 7e518a5eac13 /bin/sh
