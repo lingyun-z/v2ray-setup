83a8f5540c734764f7ab8ac611ccb88908f4bfc5
lingyun-z <zhanglingyun19970823@gmail.com>
2023-03-14 16:54:12 +0800

    1

---
+++ setup.sh
@@ -0,0 +1,4 @@
+apt update && apt install docker.io && apt install docker-compose
+
+docker network create v2ray
+sh run-crt.sh
--- start.sh
+++ start.sh
@@ -75,7 +75,4 @@ echo "
 }
 " >> config/client.json
 
-/root/.acme.sh/acme.sh --register-account -m $EMAIL --server zerossl && \
-/root/.acme.sh/acme.sh --issue -d $DOMAIN --standalone -k ec-256 && \
-/root/.acme.sh/acme.sh --installcert -d $DOMAIN --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc && \
 v2ray -config=config.json
