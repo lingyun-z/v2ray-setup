8996a88980a8e4a050bf84851fdfa1250706dddb
lingyun-z <zhanglingyun19970823@gmail.com>
2023-03-14 15:46:44 +0800

    1

--- gen-crt.sh
+++ gen-crt.sh
@@ -1,2 +1,3 @@
+/root/.acme.sh/acme.sh --register-account -m $EMAIL --server zerossl && \
 /root/.acme.sh/acme.sh --issue -d $DOMAIN --standalone -k ec-256 && \
-/root/.acme.sh/acme.sh --installcert -d $DOMAIN --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc && \
+/root/.acme.sh/acme.sh --installcert -d $DOMAIN --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
